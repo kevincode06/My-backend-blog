a71b09d948f334533580299de1b1f9e95787399d
kevlecoder <kevmal80@gmail.com>
2025-03-19 16:59:54 +0200

    added link to frontend

--- server.js
+++ server.js
@@ -10,15 +10,16 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:3000" }));
+// Allow requests from specific origin
+app.use(cors({ origin: ["http://localhost:3000", "https://my-frontend-blog-lac.vercel.app"] })); // Adjust this line to include both localhost and your deployed frontend
 app.use(express.json());
 
 // Database connection pool
 const db = mysql.createPool({
-  host:process.env.DB_HOST,
-  user:process.env.DB_USER,
-  password:process.env.DB_PASSWORD,
-  database:process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   connectTimeout: 30000,
 });
